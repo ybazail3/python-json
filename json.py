cddcb92888d63ea9627cdd8bf3dc7a4af5ffe60d
yazzy14 <yazminbazail@icloud.com>
2023-11-08 10:18:53 -0800

    first commit

---
+++ json.py
@@ -0,0 +1,13 @@
+# Importing the json class library to be able to use methods and functions in that library
+import json
+
+# Create a dictionary key: value pair for JSON
+data = {
+
+    "name": "Jane Doe",
+    "age": 25,
+    "city": "Seattle",
+    "interest": ["True crime", "video games", "working out"],
+    "is_student": True,
+
+}
