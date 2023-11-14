4580db1f9cb866d080405e71486326e40f05ca49
yazzy14 <yazminbazail@icloud.com>
2023-11-14 10:35:45 -0800

    Fixed spelling errors, and added new code to update data.json file

--- json.py
+++ json.py
@@ -12,18 +12,29 @@ data = {
 
 }
 
-# With Statement, opening a file named data.json and the w letters means we will be writing to that file. And created an object named json_files that be be. used/passed through.
+# With Statement, opening a file named data.json and the w letters means we will be writing to that file. And created an object named json_files to be used/passed through.
 with open("data.json", "w") as json_file:
     # calling the json class library/object and using the dump method from json library to add and save the data being passed through. And indent 4 spaces.
     json.dump(data, json_file, indent=4)
 
 print("Data has been written to data.json")
 
-# Opening the data.json file and reading the content and making it an object named json_file
+# Opening the data.json file and reading ("r") the content and making it an object named json_file
 with open("data.json", "r") as json_file:
-    # Created an object named loaded_data that will use the json libraray and load method to display the file
+    # Created an object named loaded_data that will use the json library and load method to display the file
     loaded_data = json.load(json_file)
 
 print("Sucessfully able to read data.json")
 # This will print the data.json file
 print(loaded_data)
+
+# Calling loaded_data and modifying the data.json file. Changing the age to 21 and adding an interest of reading to the list
+loaded_data["age"] = 21
+loaded_data["interest"].append("reading")
+
+# Writing to the data.json file and using json_file as the object
+with open("data.json", "w") as json_file:
+    # Passing through 3 args to be changed in the data.json file
+    json.dump(loaded_data, json_file, indent=4)
+
+print("Modified data written to data.json")
